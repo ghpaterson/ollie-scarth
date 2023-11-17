eb89ba7d3a5a2dc017771bc926be406b394c66b7
Graeme Paterson <ghpaterson@gmail.com>
2023-11-17 10:50:57 +0000

    added globals css styling to make gallery image and title appear on hover for mac/desktop and static for mobile

--- app/Gallery/page.js
+++ app/Gallery/page.js
@@ -18,12 +18,18 @@ export default function Gallery() {
           <h1 className="font-raleway text-3xl md:text-6xl py-6 md:py-10 md:px-6">{`GALLERY(*9)`}</h1>
         </section>
         <section className="grid grid-cols-1 md:grid-cols-12 border-t-2 border-b-2 border-blak rounded-xl gap-2 py-10 font-raleway">
-          {/* below is the template to make image appear on hover on md:screen NO hover-reveal on mobile */}
-          <div className="group md:col-span-4 bg-gray-200 h-60 hover:bg-landing-about bg-cover bg-no-repeat flex flex-col items-center justify-center text-7xl md:text-8xl">
-            <h1 className="group-hover:hidden mt-10">{`(*1)`}</h1>
-            <h1 className=" invisible group-hover:visible text-honey text-3xl py-2 px-4 bg-blak opacity-80 backdrop-blur-xl rounded-xl">
+          {/* below is the template to make image appear on hover on md:screen NO hover-reveal on mobile  note the globals.css stylesheet*/}
+          <div
+            id="gallery-card"
+            className="group md:col-span-4 bg-gray-200 h-60 hover:bg-landing-about bg-cover bg-no-repeat flex flex-col items-center justify-center text-7xl md:text-8xl"
+          >
+            <h1
+              id="gallery-card-number"
+              className="group-hover:hidden mt-10"
+            >{`(*1)`}</h1>
+            <h2 className=" text-honey text-3xl py-2 px-4 bg-blak opacity-80 backdrop-blur-xl rounded-xl">
               Image
-            </h1>
+            </h2>
           </div>
           <div className="md:col-span-4 bg-gray-200 h-60 hover:bg-gray-500"></div>
           <div className="md:col-span-4 bg-gray-200 h-60 hover:bg-gray-500"></div>
--- components/WorkSection.js
+++ components/WorkSection.js
@@ -11,51 +11,57 @@ export default function WorkSection() {
       viewport={{ once: true }}
     >
       <div className="grid grid-cols-12 pt-10 pb-10 md:py-20 border-t-2 border-b-2 border-blak rounded-xl">
+        <div
+          id="work-sub-heading"
+          className="flex justify-start pb-6 px-4 text-blak font-raleway text-3xl"
+        >
+          <h1>{`WORK(*7)`}</h1>
+        </div>
         <div className="col-span-12 flex flex-col items-start md:items-end">
           <ul className="flex flex-col gap-4 md:gap-6 items-start md:items-end font-raleway text-md md:text-6xl text-blak">
-            <li className="group hover:text-shade/90 hover:cursor-pointer hover:translate-x-6 md:hover:-translate-x-8 transition duration-500 ease-in flex">
+            <li className="group hover:text-shade/90 hover:cursor-pointer md:hover:-translate-x-8 transition duration-500 ease-in flex">
               <span className="text-xs md:text-3xl px-2">{`(*1)`}</span> BODY IN
               THE SUITCASE{" "}
               <span className="invisible group-hover:visible px-2">
                 <GoArrowUpRight />
               </span>
             </li>
-            <li className="group hover:text-shade/90 hover:cursor-pointer hover:translate-x-6 md:hover:-translate-x-8 transition duration-500 ease-in flex">
+            <li className="group hover:text-shade/90 hover:cursor-pointer md:hover:-translate-x-8 transition duration-500 ease-in flex">
               <span className="text-xs md:text-3xl px-2">{`(*2)`}</span> TALES
               FROM THE KITCHEN GARDEN{" "}
               <span className="invisible group-hover:visible px-2">
                 <GoArrowUpRight />
               </span>
             </li>
-            <li className="group hover:text-shade/90 hover:cursor-pointer hover:translate-x-6 md:hover:-translate-x-8 transition duration-500 ease-in flex">
+            <li className="group hover:text-shade/90 hover:cursor-pointer md:hover:-translate-x-8 transition duration-500 ease-in flex">
               <span className="text-xs md:text-3xl px-2">{`(*3)`}</span> OUR
               DEMENTIA CHOIR{" "}
               <span className="invisible group-hover:visible px-2">
                 <GoArrowUpRight />
               </span>
             </li>
-            <li className="group hover:text-shade/90 hover:cursor-pointer hover:translate-x-6 md:hover:-translate-x-8 transition duration-500 ease-in flex">
+            <li className="group hover:text-shade/90 hover:cursor-pointer md:hover:-translate-x-8 transition duration-500 ease-in flex">
               <span className="text-xs md:text-3xl px-2">{`(*4)`}</span> GREAT
               GARDEN REVOLUTION{" "}
               <span className="invisible group-hover:visible px-2">
                 <GoArrowUpRight />
               </span>
             </li>
-            <li className="group hover:text-shade/90 hover:cursor-pointer hover:translate-x-6 md:hover:-translate-x-8 transition duration-500 ease-in flex">
+            <li className="group hover:text-shade/90 hover:cursor-pointer md:hover:-translate-x-8 transition duration-500 ease-in flex">
               <span className="text-xs md:text-3xl px-2">{`(*5)`}</span> LIVING
               WITH A SERIAL KILLER{" "}
               <span className="invisible group-hover:visible px-2">
                 <GoArrowUpRight />
               </span>
             </li>
-            <li className="group hover:text-shade/90 hover:cursor-pointer hover:translate-x-6 md:hover:-translate-x-8 transition duration-500 ease-in flex">
+            <li className="group hover:text-shade/90 hover:cursor-pointer md:hover:-translate-x-8 transition duration-500 ease-in flex">
               <span className="text-xs md:text-3xl px-2">{`(*6)`}</span> YOUR
               GARDEN MADE PERFECT{" "}
               <span className="invisible group-hover:visible px-2">
                 <GoArrowUpRight />
               </span>
             </li>
-            <li className="group hover:text-shade/90 hover:cursor-pointer hover:translate-x-6 md:hover:-translate-x-8 transition duration-500 ease-in flex">
+            <li className="group hover:text-shade/90 hover:cursor-pointer md:hover:-translate-x-8 transition duration-500 ease-in flex">
               <span className="text-xs md:text-3xl px-2">{`(*7)`}</span> YOUR
               HOME MADE PERFECT{" "}
               <span className="invisible group-hover:visible px-2">
