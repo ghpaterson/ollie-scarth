3bdafb09ff6b883d0ac35c59960e5c73f8a7ef2f
Graeme Paterson <ghpaterson@gmail.com>
2023-11-21 17:32:40 +0000

    added warped hero Ollie Scarth to both mobile and regular screen

--- components/Hero.js
+++ components/Hero.js
@@ -5,26 +5,47 @@ import JHDunes from "../public/images/JHDunes.jpg";
 import picnic from "../public/images/picnic.JPG";
 import bicycle from "../public/images/bicycle.png";
 import { IoFlowerOutline } from "react-icons/io5";
+import { BsArrowUpRightCircle } from "react-icons/bs";
+import ollieHeroWarp from "../public/images/ollieHeroWarp.svg";
+import ollieHeroWarpSmall from "../public/images/ollieHeroWarpSmall.svg";
+import Link from "next/link";
 export default function Hero() {
   return (
     <section
       id="hero"
-      className="grid grid-cols-1 md:grid-cols-12 w-full  bg-sand rounded-t-3xl rounded-b-xl md:rounded-t-[100px] pt-10 pb-10 md:pt-10"
+      className="grid grid-cols-1 md:grid-cols-12 w-full  bg-sand rounded-t-3xl rounded-b-xl md:rounded-t-[100px] pt-4 pb-10 md:pt-10"
     >
-      <div className="md:col-span-12 bg-transparent px-6 md:px-10 py-6 flex flex-col justify-start ">
+      <div className="md:col-span-12 bg-transparent px-6 md:px-10 py-2 flex flex-col justify-start ">
         <m.div
           // initial={{ x: "100%" }}
           // animate={{ x: "0%" }}
           // transition={{ duration: 2, ease: "easeIn" }}
-          className="text-7xl md:text-[200px] font-latosce py-12 md:py-0 text-bone absolute z-10"
+          className="text-7xl md:text-[200px] font-latosce py-4 md:py-0 md:ml-12 text-bone absolute z-10"
         >
-          <div className="flex flex-col md:flex md:flex-row md:gap-12 ml-2 md:ml-0">
+          {/* Hero Text For Large Screen */}
+          <Image
+            id="warped-hero"
+            src={ollieHeroWarp}
+            width={1200}
+            alt="Ollie Scarth"
+          />
+          <Image
+            id="warped-hero-small"
+            src={ollieHeroWarpSmall}
+            width={250}
+            alt="Ollie Scarth"
+            className="md:hidden ml-8"
+          />
+          {/* <div
+            id="hero-text-small"
+            className="flex flex-col md:flex md:flex-row md:gap-12 ml-2 md:ml-0"
+          >
             <h1>OLLIE</h1>
             <h2>SCARTH</h2>
-          </div>
+          </div> */}
           {/* <h3>SAUNDERS</h3> */}
         </m.div>
-        <div className="flex flex-col items-center justify-center md:flex-row gap-20">
+        <div className="flex flex-col items-center justify-center md:flex-row gap-12">
           <m.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
@@ -36,7 +57,7 @@ export default function Hero() {
               src={bicycle}
               width={700}
               alt="Image of sand dunes"
-              className="border-4 border-bone mt-36 md:-ml-6 md:mt-14"
+              className="border-4 border-bone mt-36 md:-ml-6 md:mt-20"
             />
             <p className="text-bone text-xs py-2 flex justify-end md:px-10">
               - Peckham, South London
@@ -46,24 +67,38 @@ export default function Hero() {
             variants={heroImageFade}
             initial="hidden"
             whileInView="show"
-            className="flex flex-col justify-center md:mt-40 text-bone gap-1 text-xl font-raleway"
+            className="flex flex-col justify-end md:mt-72 text-bone gap-1 text-sm md:text-xl font-raleway md:w-[500px]"
           >
             <p>
               Ollie is a{" "}
-              <span className="md:before:text-2xl font-script">
-                Series Producer, Producer{" "}
+              <span className="md:text-2xl font-script hover:text-green-900">
+                Series Producer,{" "}
+              </span>
+              <span className="md:text-2xl font-script hover:text-green-900">
+                Producer + Director
+              </span>{" "}
+              and{" "}
+              <span className="md:text-2xl font-script hover:text-green-900">
+                Edit Producer
+              </span>{" "}
+              based in South London. With over a decade of expertise,
+              specialising in crafting elegant narratives within the realm of
+              <span className="md:text-2xl font-script mx-2 flex gap-2">
+                Documentaries{" "}
+                <Link href="/About">
+                  <BsArrowUpRightCircle />
+                </Link>{" "}
+                and
+              </span>{" "}
+              <span className="md:text-2xl font-script flex gap-2">
+                Factual Entertainment{" "}
+                <Link href="/About">
+                  <BsArrowUpRightCircle />
+                </Link>
               </span>
             </p>
-            <p>
-              <span className="md:text-2xl font-script">+ Director </span>and{" "}
-              <span className="md:text-2xl font-script"> Edit Producer</span>{" "}
-              based in
-            </p>
-            <p>South London. With over a decade of</p>
-            <p>expertise, specialising in crafting elegant</p>
-            <p>narratives within the realms of Documentaries</p>
-            <p>and Factual Entertainment</p>
-            <div className="text-7xl flex justify-end pt-10">
+
+            <div className="text-7xl flex justify-end md:-mt-4">
               <IoFlowerOutline />
             </div>
           </m.div>
