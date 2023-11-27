3c7dfa22018055ad54d907d54e3f7e5c8a2255c5
Graeme Paterson <ghpaterson@gmail.com>
2023-11-27 08:39:46 +0000

    fixed css to make Ollie Scarth visible and added first gsap yPercent animation

--- components/Loader/index.js
+++ components/Loader/index.js
@@ -1,18 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import { words } from "./data";
+import { gsap } from "gsap";
 
 import "./Loader.css";
 export default function Loader() {
+  const loaderRef = useRef(null);
+  const progressRef = useRef(null);
+  const progressNumberRef = useRef(null);
+  const wordGroupsRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(wordGroupsRef.current, {
+      yPercent: -80,
+      duration: 5,
+    });
+  }, []);
+
   return (
-    <div className="loader-wrapper">
+    <div className="loader-wrapper" ref={loaderRef}>
       <div className="loader-progressWrapper">
-        <div className="loader-progress"></div>
-        <span className="loader-progressNumber"></span>
+        <div className="loader-progress" ref={progressRef}></div>
+        <span className="loader-progressNumber" ref={progressNumberRef}></span>
       </div>
       <div className="loader">
         <div className="loader-words">
           <div className="loader-overlay"></div>
-          <div className="loader-wordsGroup">
+          <div className="loader-wordsGroup" ref={wordGroupsRef}>
             {words.map((word, index) => {
               return (
                 <span key={index} className="loader-word">
