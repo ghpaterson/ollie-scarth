a4e22bf65265392e7753350191be1c844ca1cb92
Graeme Paterson <ghpaterson@gmail.com>
2023-12-01 10:32:00 +0000

    added new documentary

--- components/WorkCard.js
+++ components/WorkCard.js
@@ -12,33 +12,35 @@ export default function WorkCard({
 }) {
   return (
     <div className="group overflow-hidden font-neueHaas   p-4 flex flex-col ">
-      <div
-        className="w-[700] h-[280px] border-2 border-red-500"
-        style={{
-          backgroundImage: `url(${creditImage.url})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      ></div>
-      <div className="px-2">
-        <div className="pt-2">
-          <h1 className="text-2xl font-neueHaasMed uppercase  text-blak">
-            {title}
-            <span className="text-red-500">.</span>
-          </h1>
-        </div>
-        <div>
-          <h2 className="text-xl  text-blak">{channel}</h2>
-        </div>
-        <div className="flex justify-end gap-10 text-blak">
-          <p>{description}</p>
-          <div className="text-5xl flex justify-end items-end">
-            <Link href={`/Work/${category.categoryName}/${slug}`}>
-              <BsArrowUpRightCircle />
-            </Link>
+      <Link href={`/Work/${category.categoryName}/${slug}`}>
+        <div
+          className="w-[700] h-[280px] border-2 border-red-500"
+          style={{
+            backgroundImage: `url(${creditImage.url})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        <div className="px-2">
+          <div className="pt-2">
+            <h1 className="text-2xl font-neueHaasMed uppercase  text-blak">
+              {title}
+              <span className="text-red-500">.</span>
+            </h1>
+          </div>
+          <div>
+            <h2 className="text-xl  text-blak">{channel}</h2>
+          </div>
+          <div className="flex justify-end gap-10 text-blak">
+            <p>{description}</p>
+            <div className="text-5xl flex justify-end items-end">
+              <Link href={`/Work/${category.categoryName}/${slug}`}>
+                <BsArrowUpRightCircle />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
