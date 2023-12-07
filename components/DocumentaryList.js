61824961b2704630f4602dba05bc9dc226a68e3f
Graeme Paterson <ghpaterson@gmail.com>
2023-12-07 11:15:41 +0000

    adjusted documentaryList for mobile

--- components/DocumentaryList.js
+++ components/DocumentaryList.js
@@ -13,7 +13,7 @@ export default function DocumentaryList() {
   };
 
   return (
-    <main id="documentary-list-section">
+    <main id="documentary-list-section" className="mt-10 md:mt-0">
       <div className="flex justify-center gap-6 font-neueHaasMed  text-blak  text-3xl md:text-9xl uppercase md:py-20">
         <Link href="/Work/documentary" alt="link to documentary credits">
           <h1>
@@ -21,7 +21,7 @@ export default function DocumentaryList() {
           </h1>
         </Link>
       </div>
-      <div className=" w-full flex flex-col md:flex-row items-start md:justify-between md:items-center md:px-10 pb-10">
+      <div className=" w-full flex flex-col md:flex-row items-center md:justify-between md:items-center md:px-10 py-10 md:pb-10">
         <div id="documentary-list-image" className="">
           <div
             className=" border-2 border-blak"
@@ -46,7 +46,7 @@ export default function DocumentaryList() {
           </p>
         </div>
 
-        <ul className="flex flex-col gap-4 md:gap-6 items-start md:items-end font-neueHaasMed text-md md:text-3xl text-blak">
+        <ul className="flex flex-col gap-4 md:gap-6 items-center md:items-end font-neueHaasMed text-md md:text-3xl text-blak">
           {documentaryData.map((item) => (
             <m.li
               variants={elementFade}
