39f94be1376bb75b5ae8d01a47c2c1122cd381e7
Graeme Paterson <ghpaterson@gmail.com>
2023-12-07 14:57:04 +0000

    added gallery model from hygraph and rendering gallery of images on gallery page

---
+++ app/Gallery/galleryImages.js
@@ -0,0 +1,68 @@
+async function getGalleryData() {
+  const response = await fetch(process.env.HYGRAPH_ENDPOINT, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `
+      query Galleries {
+        galleries {
+          id
+          title
+         galleryImage{
+            url
+         }
+        }
+      }
+     
+        `,
+    }),
+  });
+  console.log("response status:", response.status);
+  const json = await response.json();
+  console.log("data:", json.data.galleries);
+  return json.data.galleries;
+}
+
+export default async function GalleryImages() {
+  try {
+    const galleries = await getGalleryData();
+
+    if (!galleries || galleries.length === 0) {
+      // Handle the case where galleries is undefined or an empty array
+      return (
+        <section>
+          <h1>Gallery</h1>
+          <p>No galleries available.</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="px-10">
+        {galleries.map((gallery, galleryIndex) => (
+          <div key={galleryIndex}>
+            <div className="grid grid-cols-3 gap-4">
+              {gallery.galleryImage.map((image, index) => (
+                <img
+                  key={index}
+                  src={image.url}
+                  alt={`Gallery Image ${index + 1}`}
+                  className=""
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+      </section>
+    );
+  } catch (error) {
+    // Handle errors from the API request
+    console.error("Error fetching gallery data:", error);
+    return (
+      <section>
+        <h1>Gallery</h1>
+        <p>Error fetching gallery data. Please try again later.</p>
+      </section>
+    );
+  }
+}
--- app/Gallery/page.js
+++ app/Gallery/page.js
@@ -1,28 +1,30 @@
-"use client";
+// "use client";
 
 import ScrollToTop from "@/utils/scrollToTop";
 import { motion as m, AnimatePresence } from "framer-motion";
 import { pageTransition } from "@/utils/motions";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import GalleryImages from "./galleryImages";
+
 export default function Gallery() {
   return (
-    <AnimatePresence mode="wait">
-      <main>
-        <NavBar />
-        <m.section
-          variants={pageTransition}
-          initial="hidden"
-          whileInView="show"
-          className="pb-6"
-        >
-          <section className="flex justify-center">
-            <h1 className="font-neueHaasMed uppercase text-3xl md:text-6xl py-6 md:py-10 md:px-6">
-              gallery<span className="text-red-500">.</span>
-            </h1>
-          </section>
-          <section className="grid grid-cols-1 md:grid-cols-12  rounded-xl gap-2 py-10 font-neueHaas px-10">
-            {/* below is the template to make image appear on hover on md:screen NO hover-reveal on mobile  note the globals.css stylesheet*/}
+    // <AnimatePresence mode="wait">
+    <main>
+      <NavBar />
+      <section
+        // variants={pageTransition}
+        // initial="hidden"
+        // whileInView="show"
+        className="pb-6"
+      >
+        <section className="flex justify-center">
+          <h1 className="font-neueHaasMed uppercase text-3xl md:text-6xl py-6 md:py-10 md:px-6">
+            gallery<span className="text-red-500">.</span>
+          </h1>
+        </section>
+        <GalleryImages />
+        {/* <section className="grid grid-cols-1 md:grid-cols-12  rounded-xl gap-2 py-10 font-neueHaas px-10">
             <div
               id="gallery-card"
               className="group md:col-span-4 bg-gray-200 h-60 hover:bg-landing-about bg-cover bg-no-repeat flex flex-col items-center justify-center text-7xl md:text-8xl"
@@ -40,11 +42,11 @@ export default function Gallery() {
             <div className="md:col-span-4 bg-gray-200 h-60 hover:bg-gray-500"></div>
             <div className="md:col-span-4 bg-gray-200 h-60 hover:bg-gray-500"></div>
             <div className="md:col-span-4 bg-gray-200 h-60 hover:bg-gray-500"></div>
-          </section>
-        </m.section>
-        <ScrollToTop />
-        <Footer />
-        <m.div
+          </section> */}
+      </section>
+      <ScrollToTop />
+      <Footer />
+      {/* <m.div
           className="slide-in"
           initial={{ scaleY: 0 }}
           animate={{ scaleY: 0 }}
@@ -57,8 +59,8 @@ export default function Gallery() {
           animate={{ scaleY: 0 }}
           exit={{ scaleY: 0 }}
           transition={{ duration: 1.5, ease: [0.22, 1, 0.36, 1] }}
-        ></m.div>
-      </main>
-    </AnimatePresence>
+        ></m.div> */}
+    </main>
+    // </AnimatePresence>
   );
 }
